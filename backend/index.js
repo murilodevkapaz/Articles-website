9915c93a913b2cd0649bf2dd1fc5d306decdea35
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2020-11-02 15:40:55 -0300

    passport inserted

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const db = require('./config/db');
 app.db = db;
 //com isso não precisso dar require('express') nas outras pastas basta module.exports=> app => app.use..
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validator.js')
     .then('./api') //todos os arquivos que estão dentro da pasta api
