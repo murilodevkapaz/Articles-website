cfd3a41d2af6f3e135081435efa11e531bf8c2f1
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2020-11-08 10:45:35 -0300

    validate signup

--- backend/api/user.js
+++ backend/api/user.js
@@ -12,6 +12,7 @@ module.exports = app => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
 
+        //so cadastra usuario admin pela url /users
         if(!req.originalUrl.startsWith('/users')) user.admin = false
         if(!req.user || !req.user.admin) user.admin = false
 
