bf8ef66707fc8a789a2a467d938d5d3b5ff6901a
Murilo <murilodevkapaz@gmail.com>
2020-11-26 11:49:34 -0300

    pagination in articles added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router';
 Vue.config.productionTip = false
 
 //TEMPORÁRIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik11cmlsbyBSZXJpc29uIGRlIFNvdXNhIFBheiIsImVtYWlsIjoibXVyaWxva2FwYXpAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYwNTk5Mzc2MSwiZXhwIjoxNjA2MjUyOTYxfQ.lupuZmObv-sZvp-RD2WrJAV0oL65hykHCpRLi-fTXA4';
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik11cmlsbyBSZXJpc29uIGRlIFNvdXNhIFBheiIsImVtYWlsIjoibXVyaWxva2FwYXpAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYwNjM1NjYwNiwiZXhwIjoxNjA2NjE1ODA2fQ.UiHVbH4pOVUKlt9fmYGd5LE5xF2XTs0eOW2N62luYxE';
 
 new Vue({
   store,
